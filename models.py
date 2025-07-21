da1d0bed33bb0a8fc8c60cb78ce53e8797186844
Marisa <marisa.meurer@gmail.com>
2025-07-21 12:36:02 -0300

    Atualizar o models.py

--- models.py
+++ models.py
@@ -4,3 +4,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
+    binance_api_key = db.Column(db.String(200), nullable=True)
+    binance_api_secret = db.Column(db.String(200), nullable=True)
+    openai_api_key = db.Column(db.String(200), nullable=True)
