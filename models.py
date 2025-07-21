3f5251e82fb7fb51b0e565dc8e59f31b30dc3b17
Marisa <marisa.meurer@gmail.com>
2025-07-21 20:29:38 -0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    email = db.Column(db.String(150), nullable=False, unique=True)
-    binance_api_key = db.Column(db.String(200), nullable=True)
-    binance_api_secret = db.Column(db.String(200), nullable=True)
-    openai_api_key = db.Column(db.String(200), nullable=True)
