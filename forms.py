ec4222aa60ce89948ebf2d1375e9c4b5b7f4c561
Marisa <marisa.meurer@gmail.com>
2025-07-21 12:36:35 -0300

    Atualizar o forms.py

--- forms.py
+++ forms.py
@@ -11,3 +11,9 @@ class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class ApiKeysForm(FlaskForm):
+    binance_api_key = StringField('Binance API Key', validators=[DataRequired()])
+    binance_api_secret = StringField('Binance API Secret', validators=[DataRequired()])
+    openai_api_key = StringField('OpenAI API Key', validators=[DataRequired()])
+    submit = SubmitField('Salvar Chaves')
