7e802e7fc60f6a1e88f2cdd17fd5e07d02098313
Marisa <marisa.meurer@gmail.com>
2025-08-09 02:43:41 -0300

    Atualizar o rotas.py

--- usuarios/rotas.py
+++ usuarios/rotas.py
@@ -11,17 +11,13 @@ def login():
     if request.method == "POST":
         username = request.form.get("username", "").strip()
         password = request.form.get("password", "").strip()
-
         with SessionLocal() as s:
             user = s.query(Usuario).filter(Usuario.username == username).one_or_none()
             if not user or not check_password_hash(user.password_hash, password):
                 flash("Usuário ou senha inválidos.", "danger")
                 return redirect(url_for(".login"))
-
         login_user(user, remember=True)
         return redirect(url_for("painel_operacao.painel_operacao"))
-
-    # GET -> exibe formulário
     return render_template("index.html")
 
 @bp.route("/logout")
